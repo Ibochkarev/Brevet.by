482acecb24d3c7b06aa6c5ba06f1c2d39c8b0559
Ivan Bochkarev <ivanx86@gmail.com>
2019-06-18 17:01:02 +0600

    Add new page

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+  $(function () {
 
+    $('.menuToggle').on('click', function () {
+      $('.menu').slideToggle(300, function () {
+        if ($(this).css('display') === "none") {
+          $(this).removeAttr('style');
+        }
+      });
+
+    });
+
+  });
 });
